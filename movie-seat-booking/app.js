86902ef4e96b8a65d654fb4ed95d75344eaba59e
ughdeeb <adeebsyed725@gmail.com>
2021-04-25 16:44:28 -0400

    UI completed

---
+++ movie-seat-booking/app.js
@@ -0,0 +1,32 @@
+const container = document.querySelector(".container");
+const seats = document.querySelectorAll(".row .seat:not(.occupied)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
+
+let ticketPrice = parseInt(movieSelect.value);
+
+function updateSelectedCount() {
+  const selectedSeats = document.querySelectorAll(".row .seat.selected");
+  const selectedSeatsCount = selectedSeats.length;
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+//MOVIE SELECT EVENT
+movieSelect.addEventListener("change", (e) => {
+  ticketPrice = +e.target.value; //+ sign also makes its a number like parseInt
+  updateSelectedCount();
+});
+
+//SEAT CLICK EVENT
+container.addEventListener("click", (e) => {
+  if (
+    e.target.classList.contains("seat") &&
+    !e.target.classList.contains("occupied")
+  ) {
+    e.target.classList.toggle("selected");
+
+    updateSelectedCount();
+  }
+});
